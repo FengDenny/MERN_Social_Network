ab0af018fad790a970ef436ea9e41e5ac8dbe831
Denny Feng <dfeng1@mail.sfsu.edu>
2021-01-18 18:52:13 -0800

    DB connection

--- server.js
+++ server.js
@@ -11,6 +11,28 @@ process.on("uncaughtException", (err) => {
 
 dotenv.config({ path: "./config/config.env" });
 
+// Database connection
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    console.log(`Connected to MongoDB: ${process.env.DATABASE} successfully!`)
+  );
+
+//  port declarations
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+  console.log(`App is running on port ${port} in ${process.env.NODE_ENV} mode`);
+});
+
 // Catching rejections
 process.on("uncaughtRejection", (err) => {
   console.log("UNCAUGHT REJECTION");
