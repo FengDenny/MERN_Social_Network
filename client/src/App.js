77144f9aaf6b626584f2300182af2fdb70cd7584
Denny Feng <dfeng1@mail.sfsu.edu>
2021-01-19 22:36:13 -0800

    Footer

--- client/src/App.js
+++ client/src/App.js
@@ -8,12 +8,14 @@ import Footer from "./utility/Footer/Footer";
 function App() {
   return (
     <Router>
-      <div className='App'>
-        {/* <Route path='/' component={Navbar} /> */}
-        <Switch>
-          <Route exact path='/' component={Home} />
-        </Switch>
-        {/* <Route path='/' component={Footer} /> */}
+      <div className='App '>
+        <div className='App-content'>
+          {/* <Route path='/' component={Navbar} /> */}
+          <Switch>
+            <Route exact path='/' component={Home} />
+          </Switch>
+        </div>
+        <Route path='/' component={Footer} />
       </div>
     </Router>
   );
