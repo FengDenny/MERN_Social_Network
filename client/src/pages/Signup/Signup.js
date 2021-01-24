ad1c160f0679273f9e10ee6638d4c19024e83d95
Denny Feng <dfeng1@mail.sfsu.edu>
2021-01-23 17:41:08 -0800

    Post request from backend

--- client/src/pages/Signup/Signup.js
+++ client/src/pages/Signup/Signup.js
@@ -1,11 +1,57 @@
-import React from "react";
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import Login from "../../pages/Login/Login";
 import openModal from "../../actions/openModal";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
+import { showAlert } from "../../js/alerts";
+import axios from "axios";
 
 function Signup(props) {
+  const [values, setValues] = useState({
+    name: "Denny",
+    email: "Dfeng415@yahoo.com",
+    password: "123456aA!",
+  });
+  const { name, email, password } = values;
+
+  const handleChanges = (name) => (event) => {
+    // grab different values based on their name located in  values
+    // combining/including all elements from an object/array to setValues using spread values (...values)
+    setValues({ ...values, [name]: event.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const url = `/api/v1/auth/signup`;
+    const data = { name, email, password };
+
+    // post request from the backend
+    await axios
+      .post(url, data)
+      .then((res) => {
+        console.log("SIGN UP SUCCESSFULLY", res);
+        console.log(res.data.message);
+        // clean up the state
+        setValues({
+          name: "",
+          email: "",
+          password: "",
+        });
+
+        // show success alert
+
+        if (res.data.status === "success") {
+          showAlert("success", res.data.message);
+        }
+      })
+      .catch((err) => {
+        console.log("SIGN UP ERROR,", err.response.data.message);
+        showAlert("error", err.response.data.message);
+      });
+  };
+
   return (
     <form>
       <div className='form-group flex-direction-column '>
@@ -15,7 +61,14 @@ function Signup(props) {
         >
           Full Name
         </label>
-        <input id='name' type='text' className='form-input' name='name' />
+        <input
+          id='name'
+          type='text'
+          className='form-input'
+          name='name'
+          onChange={handleChanges("name")}
+          value={name}
+        />
       </div>
       <div className='form-group flex-direction-column margin-top '>
         <label
@@ -24,7 +77,14 @@ function Signup(props) {
         >
           Email
         </label>
-        <input id='email' type='text' className='form-input' name='email' />
+        <input
+          id='email'
+          type='text'
+          className='form-input'
+          name='email'
+          onChange={handleChanges("email")}
+          value={email}
+        />
       </div>
       <div className='form-group flex-direction-column margin-top '>
         <label
@@ -38,10 +98,17 @@ function Signup(props) {
           type='password'
           className='form-input'
           name='password'
+          onChange={handleChanges("password")}
+          value={password}
         />
       </div>
       <div className='form-group flex-direction-column margin-top '>
-        <button className='btn-primary green-background md'>Sign up</button>
+        <button
+          className='btn-primary green-background md'
+          onClick={handleSubmit}
+        >
+          Sign up
+        </button>
       </div>
       <div className='form-group flex-direction-column margin-top '>
         <p className='secondary-heading secondary-color sm'>
