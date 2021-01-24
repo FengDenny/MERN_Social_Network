40b0429434013453f53dd64e0b2774e147be0aeb
Denny Feng <dfeng1@mail.sfsu.edu>
2021-01-23 17:39:44 -0800

    Account Activation page

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import "./index.css";
 import "./css/mediaQueries.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home/Home";
+import Activate from "./pages/Signup/AccountActivation";
 import Navbar from "./utility/Navbar/Navbar";
 import Footer from "./utility/Footer/Footer";
 import Modal from "./utility/Modal/Modal";
@@ -15,6 +16,7 @@ function App() {
           {/* <Route path='/' component={Navbar} /> */}
           <Switch>
             <Route exact path='/' component={Home} />
+            <Route exact path='/auth/activate/:token' component={Activate} />
           </Switch>
         </div>
         <Route path='/' component={Modal} />
