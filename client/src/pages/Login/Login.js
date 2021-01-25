a2ce1f6e2e5ca2ea7600a5170908119eefe482bc
Denny Feng <dfeng1@mail.sfsu.edu>
2021-01-24 22:18:29 -0800

    Authentication

--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -1,13 +1,21 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, Redirect, withRouter } from "react-router-dom";
 import { showLoginAlert } from "../../js/alerts";
+import { authenticate, isAuth } from "../../utility/helper/helpers";
 import axios from "axios";
 
-function Login() {
+function Login({ history }) {
   const [values, setValues] = useState({
-    email: "",
-    password: "",
+    email: "dfeng415@yahoo.com",
+    password: "123456aA!",
+    redirectToReferer: false,
   });
+
+  // close modal
+  const reload = () => {
+    window.location.reload();
+  };
 
   const { email, password } = values;
 
@@ -15,74 +23,101 @@ function Login() {
     // grab different values based on their name located in  values
     setValues({ ...values, [name]: event.target.value });
   };
+
   const handleSubmit = async (e) => {
     e.preventDefault();
-
     const url = "/api/v1/auth/signin";
     const data = { email, password };
-
-    axios
+    // post request from backend
+    await axios
       .post(url, data)
       .then((res) => {
-        console.log("SIGN IN SUCCESSFULLY", res);
-        if (res.data.status === "success") {
-          showLoginAlert("success", `Hey ${res.data.user.name}, Welcome back!`);
-        }
+        console.log("LOG IN SUCCESSFULLY", res);
+        authenticate(res, () => {
+          // clean state
+          setValues({
+            ...values,
+            email: "",
+            password: "",
+          });
+          // if (res.data.status === "success") {
+          //   reload();
+          // }
+          isAuth() && isAuth().role === "admin"
+            ? history.push("/admin")
+            : history.push("/subscriber");
+        });
       })
+
       .catch((err) => {
-        console.log("SIGN UP ERROR", err);
+        console.log("LOG IN ERROR", err);
         showLoginAlert("error", err.response.data.message);
       });
   };
 
+  const LoginForm = () => {
+    return (
+      <form className='form'>
+        <h1 className='primary-color flex justify-center '>Welcome back.</h1>
+        <div className='form-group flex-direction-column margin-top '>
+          <label
+            htmlFor='email'
+            className='label-name secondary-heading secondary-color md'
+          >
+            Email
+          </label>
+          <input
+            id='email'
+            type='text'
+            className='form-input'
+            name='email'
+            onChange={handleChanges("email")}
+            value={email}
+          />
+        </div>
+        <div className='form-group flex-direction-column margin-top-30 '>
+          <label
+            htmlFor='password'
+            className='label-name secondary-heading secondary-color md'
+          >
+            Password
+          </label>
+          <Link className='password-recovery' to=''>
+            Forgot your password?
+          </Link>
+          <input
+            id='password'
+            type='password'
+            className='form-input'
+            name='password'
+            onChange={handleChanges("password")}
+            value={password}
+          />
+        </div>
+        <div className='form-group flex-direction-column margin-top-30 '>
+          <button
+            className='link-btn primary-background btn-primary md'
+            onClick={handleSubmit}
+          >
+            Login
+          </button>
+        </div>
+      </form>
+    );
+  };
+
   return (
-    <form className='form'>
-      <h1 className='primary-color flex justify-center '>Welcome back.</h1>
-      <div className='form-group flex-direction-column margin-top '>
-        <label
-          htmlFor='email'
-          className='label-name secondary-heading secondary-color md'
-        >
-          Email
-        </label>
-        <input
-          id='email'
-          type='text'
-          className='form-input'
-          name='email'
-          onChange={handleChanges("email")}
-          value={email}
-        />
-      </div>
-      <div className='form-group flex-direction-column margin-top-30 '>
-        <label
-          htmlFor='password'
-          className='label-name secondary-heading secondary-color md'
-        >
-          Password
-        </label>
-        <Link className='password-recovery' to=''>
-          Forgot your password?
-        </Link>
-        <input
-          id='password'
-          type='password'
-          className='form-input'
-          name='password'
-          onChange={handleChanges("password")}
-          value={password}
-        />
-      </div>
-      <div className='form-group flex-direction-column margin-top-30 '>
-        <button
-          className='link-btn primary-background btn-primary md'
-          onClick={handleSubmit}
-        >
-          Login
-        </button>
-      </div>
-    </form>
+    <div>
+      {/* check if user is signed in  */}
+      {isAuth() && isAuth().role === "admin" ? (
+        <Redirect to='/admin' />
+      ) : isAuth() && isAuth().role === "subscriber" ? (
+        <Redirect to='/subscriber' />
+      ) : null}
+
+      {LoginForm()}
+    </div>
   );
 }
 
-export default Login;
+export default withRouter(Login);
