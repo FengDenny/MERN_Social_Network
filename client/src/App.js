bd189a86f0b81c77da0176650212ef786992a5c3
Denny Feng <dfeng1@mail.sfsu.edu>
2021-01-24 22:17:11 -0800

    Admin/Subscriber route

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,12 @@ import "./css/mediaQueries.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Activate from "./pages/Signup/AccountActivation";
+import SubscriberRoute from "./pages/Subscriber/SubscriberRoute";
+import Subscriber from "./pages/Subscriber/Subscriber";
+import AdminRoute from "./pages/Admin/AdminRoute";
+import Admin from "./pages/Admin/Admin";
 import Navbar from "./utility/Navbar/Navbar";
+import Feeds from "./pages/Feeds/Feeds";
 import Footer from "./utility/Footer/Footer";
 import Modal from "./utility/Modal/Modal";
 
@@ -13,10 +18,14 @@ function App() {
     <Router>
       <div className='App '>
         <div className='App-content'>
-          {/* <Route path='/' component={Navbar} /> */}
+          <Route path='/feeds' component={Navbar} />
           <Switch>
             <Route exact path='/' component={Home} />
             <Route exact path='/auth/activate/:token' component={Activate} />
+            <Route exact path='/feeds' component={Feeds} />
+            {/* Private/protected */}
+            <SubscriberRoute exact path='/subscriber' component={Subscriber} />
+            <AdminRoute exact path='/admin' component={Admin} />
           </Switch>
         </div>
         <Route path='/' component={Modal} />
