34421df5b26e22ef6a9b4f78a6d3d4b2fdcebab7
Denny Feng <dfeng1@mail.sfsu.edu>
2021-01-26 19:04:43 -0800

    Refactored

--- client/src/utility/Navbar/Navbar.js
+++ client/src/utility/Navbar/Navbar.js
@@ -1,8 +1,8 @@
-import React, { Fragment } from "react";
-import { NavLink, withRouter } from "react-router-dom";
-import { isAuth, logout } from "../helper/helpers";
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+import Dropdown from "./Dropdown";
 
-function Navbar({ history }) {
+function Navbar() {
   return (
     <nav className='navbar primary-background  '>
       <div className='container '>
@@ -10,49 +10,10 @@ function Navbar({ history }) {
           <h1 className='nav-logo primary-heading  '>MSNwork.</h1>
         </header>
         <ul className='nav-links flex justify-end md'>
-          {!isAuth() && (
-            <Fragment>
-              <li className='nav-item'>
-                <NavLink
-                  activeStyle={{
-                    borderBottom: "1px solid #db4437 ",
-                  }}
-                  exact
-                  to='/'
-                >
-                  Signup
-                </NavLink>
-              </li>
-              <li className='nav-item'>
-                <NavLink
-                  activeStyle={{
-                    borderBottom: "1px solid #db4437 ",
-                  }}
-                  exact
-                  to='/'
-                >
-                  Login
-                </NavLink>
-              </li>
-            </Fragment>
-          )}
-          {isAuth() && (
-            <li className='nav-item'>
-              <span
-                className='span-link'
-                onClick={() => {
-                  logout(() => {
-                    history.push("/");
-                  });
-                }}
-              >
-                Logout
-              </span>
-            </li>
-          )}
+          <Dropdown />
         </ul>
       </div>
     </nav>
   );
 }
-export default withRouter(Navbar);
+export default Navbar;
