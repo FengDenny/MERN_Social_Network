94dd8717cb4f25e94810a25cec51b4057e57dc61
Denny Feng <dfeng1@mail.sfsu.edu>
2021-01-26 19:03:09 -0800

    Refactored pages

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,12 @@ import "./App.css";
 import "./index.css";
 import "./css/mediaQueries.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Home from "./pages/Home/Home";
+import AuthHome from "./pages/AuthHome/AuthHome";
+import UserHome from "./pages/UserHome/UsersHome";
 import Activate from "./pages/Signup/AccountActivation";
-import SubscriberRoute from "./pages/Subscriber/SubscriberRoute";
-import Subscriber from "./pages/Subscriber/Subscriber";
-import AdminRoute from "./pages/Admin/AdminRoute";
-import Admin from "./pages/Admin/Admin";
+import AuthRoute from "./pages/AuthHome/AuthRoute";
+import Profile from "./pages/Profile/Profile";
 import Navbar from "./utility/Navbar/Navbar";
-import Feeds from "./pages/Feeds/Feeds";
 import Footer from "./utility/Footer/Footer";
 import Modal from "./utility/Modal/Modal";
 
@@ -18,14 +16,17 @@ function App() {
     <Router>
       <div className='App '>
         <div className='App-content'>
-          <Route path='/feeds' component={Navbar} />
+          <Route path='/user' component={Navbar} />
           <Switch>
-            <Route exact path='/' component={Home} />
+            <Route exact path='/' component={AuthHome} />
+            <Route exact path='/user' component={UserHome} />
             <Route exact path='/auth/activate/:token' component={Activate} />
-            <Route exact path='/feeds' component={Feeds} />
             {/* Private/protected */}
-            <SubscriberRoute exact path='/subscriber' component={Subscriber} />
-            <AdminRoute exact path='/admin' component={Admin} />
+            <AuthRoute
+              exact
+              path='/user/profile-setting/:id'
+              component={Profile}
+            />
           </Switch>
         </div>
         <Route path='/' component={Modal} />
