9ace38dab515c2280bfe3b5168130e6dccb6d165
FengDenny <fengdennyy@gmail.com>
2021-02-01 17:52:42 -0800

    Retreiving data from authenticated/ non authenticated  users

--- client/src/pages/Profile/UserProfile.js
+++ client/src/pages/Profile/UserProfile.js
@@ -2,10 +2,13 @@
 import React, { useState, useEffect } from "react";
 import { isAuth, getCookie } from "../../utility/helper/helpers";
 import { withRouter } from "react-router-dom";
+import DeleteUserProfile from "./DeleteUserProfile";
 import axios from "axios";
 function UserProfile({ match, history }) {
   const [data, setData] = useState({
     created: "",
+    name: "",
+    emial: "",
   });
   const token = getCookie("token");
   //   userId from App.js: /user/:userId
@@ -25,8 +28,8 @@ function UserProfile({ match, history }) {
     })
       .then((response) => {
         console.log("PRIVATE PROFILE", response.data);
-        const { created } = response.data;
-        setData({ ...data, created });
+        const { created, name, email } = response.data;
+        setData({ ...data, created, name, email });
       })
       .catch((error) => {
         if (error) {
@@ -35,21 +38,27 @@ function UserProfile({ match, history }) {
       });
   };
 
-  const { created } = data;
+  const { created, name, email } = data;
   return (
     <div className='container'>
-      <div className='margin-top secondary-color   '>
-        <h1 className='primary-color '>User's Profile</h1>
-        <p className='margin-top md '>Welcome, {isAuth().name} </p>
-        <p className='md '>Email: {isAuth().email}</p>
-        <p className='md  '>{`Joined: ${new Date(created).toDateString()}`}</p>
-        {isAuth() && isAuth()._id === id && (
-          <div className='inline-block margin-top'>
-            <button className='danger-background white-color btn-primary width-md md '>
-              Delete Profile
-            </button>
-          </div>
-        )}
+      <div className='margin-top secondary-color '>
+        <h1 className='primary-color flex flex-direction-column '>
+          User's Profile
+        </h1>
+
+        <div className='user-profile-img margin-top-30'>
+          <img
+            src='https://res.cloudinary.com/dis7ep3yq/image/upload/v1607809957/1024px-No_image_available.svg_p8eu6x.png'
+            alt='profile'
+          />
+        </div>
+
+        <div className=' user-profile-info'>
+          <p className='margin-top l '>Name: {name} </p>
+          <p className='l '>Email: {email}</p>
+          <p className='l  '>{`Joined: ${new Date(created).toDateString()}`}</p>
+          {isAuth() && isAuth()._id === id && <DeleteUserProfile />}
+        </div>
       </div>
     </div>
   );
